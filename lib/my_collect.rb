e651d4d50dfc51e8fd33d1a00693058b0fc32cd1
Emilio Betancourt <embeor@gmail.com>
2018-10-06 20:40:43 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
 
+def my_collect (argument)
+  i = 0
+  new_array = []
+  while argument.length > i
+      new_array.push(yield argument[i])
+      i +=1
+  end
+  new_array
+end
+
+my_collect(array) do |name|
+  name.split(" ").first
+end
